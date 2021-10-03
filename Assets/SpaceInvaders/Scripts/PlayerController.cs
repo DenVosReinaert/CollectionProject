14488062701dac31e962aa1bc87399830f696dbb
Adriaan Pronk <adriaan.fazant@live.nl>
2021-10-03 11:27:37 +0100

    Player Controller

--- Assets/SpaceInvaders/Scripts/PlayerController.cs
+++ Assets/SpaceInvaders/Scripts/PlayerController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;
+    public float lives = 3f;
+    public GameObject bullet;
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space) && !bullet.activeInHierarchy)
+        {
+            bullet.transform.position = this.transform.position;
+            bullet.SetActive(true);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        this.GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxisRaw("Horizontal"), 0) * speed;
     }
 }
